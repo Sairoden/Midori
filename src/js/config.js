3b6574851c66994a6c3fd751a8d0407e51c0af6b
Sairoden Gandarosa <72735313+Sairoden@users.noreply.github.com>
2021-04-12 12:39:35 +0800

    Add files via upload
    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `c5ef1af6-c739-439f-aac8-bdbb03587dff`;
+export const MODAL_CLOSE_SEC = 2.5;
